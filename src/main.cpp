a2d2000bf323c85cf2d50a6be411256f3093410d
sgrsn <sgrsn1711@gmail.com>
2024-06-04 18:00:08 +0900

    Implement joystick server

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,38 @@
 #include <WiFi.h>
 #include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
 #include <ESP32Servo.h>
-#include "index_html.h"
+#include <WebServer.h>
+#include <stdio.h>
+#include <SPI.h>
+#include <WiFiUdp.h>
+#include "SPIFFS.h"
 
 /* WiFi */
 const char *ssid = "ESP32ap";
 const char *password = "12345678";
 const IPAddress ip(192, 168, 10, 1);
 const IPAddress subnet(255, 255, 255, 0); 
+WiFiUDP udp;
+WebServer server(80);
 
-/* ESC */
-Servo servo1;
-const int esc_pin = 21;
-const int led_pin = 22;
-int minUs = 800;
-int maxUs = 2400;
+int left_x;
+int left_y;
+int right_x;
+int right_y;
 
-/* Watchdogtimer */
-unsigned long http_watchdog_time = 0;
+void handleData() {
+  left_x = server.arg("leftX").toInt();
+  left_y = server.arg("leftY").toInt();
+  right_x = server.arg("rightX").toInt();
+  right_y = server.arg("rightY").toInt();
 
-/* HTTP Server */
-AsyncWebServer server(80);
-const char* index_html = INDEX_HTML; // index_html.hより読み込み
-const char* input_parameter = "value";
-
-/* Timer (watchdogtimer) */
-hw_timer_t * timer = NULL;
-void IRAM_ATTR onTimer() {
-  http_watchdog_time++;
-  if (http_watchdog_time > 5)
-  {
-    servo1.writeMicroseconds(minUs);
-    digitalWrite(led_pin, !digitalRead(led_pin));
-  }
-  else{
-    digitalWrite(led_pin, LOW);
-  }
+  /* response to client*/
+  server.send(200, "text/plain", "");
 }
 
-/* XMLHttpRequest Set initial value of slider */
-String processor(const String& var){
-  if (var == "SLIDERVALUE"){
-    return "0";
-  }
-  return String();
-}
-
-void setup(){
+void setup() {
   /* Serial */
   Serial.begin(9600);
-  
-  pinMode(led_pin, OUTPUT);
-  digitalWrite(led_pin, LOW);
-
-  /* Timer Setup */
-  timer = timerBegin(0, 80, true);
-  timerAttachInterrupt(timer, &onTimer, true);
-  timerAlarmWrite(timer, 100000, true);
-  timerAlarmEnable(timer);
-
   /* Wifi access point Setup */
   Serial.println();
   Serial.print("Configuring access point...");
@@ -69,32 +42,31 @@ void setup(){
   Serial.print("AP IP address: ");
   Serial.println(myIP);
 
-  /* Server Setup */
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });
-
-  server.on("/slider", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    String message;
-    if (request->hasParam(input_parameter)) {
-      message = request->getParam(input_parameter)->value();
-      servo1.writeMicroseconds(map(message.toInt(), 0, 255, minUs, maxUs));
-    }
-    else {
-      message = "No message sent";
-    }
-    request->send(200, "text/plain", "OK");
-    http_watchdog_time = 0;
-  });
+  /* UDP */
+  unsigned int localUdpPort = 4210;
+  Serial.println("begin UDP port");
+  udp.begin(localUdpPort);
+  Serial.print("local UDP port: ");
+  Serial.println(localUdpPort);
   
+  /* SPIFFS */
+  if (!SPIFFS.begin()) {
+    Serial.println("SPIFFS failed to mount");
+    Serial.println("Please run following command to format SPIFFS:");
+    Serial.println("\033[31mpio run -t uploadfs\033[0m");
+  } else {
+    Serial.println("SPIFFS OK");
+  }
+
+  /* WebServer */
+  server.serveStatic("/", SPIFFS, "/joystick.html");
+  server.serveStatic("/virtualjoystick.js", SPIFFS, "/virtualjoystick.js");
+  server.on("/index.html", handleData);
   server.begin();
-
-  // Calibration ESC
-  ESP32PWM::allocateTimer(0);
-  servo1.setPeriodHertz(50);      // Standard 50hz servo
-  servo1.attach(esc_pin, minUs, maxUs);
-  servo1.writeMicroseconds(minUs);
 }
-  
+
 void loop() {
+  server.handleClient();
+  Serial.printf("left X: %d left Y: %d right X: %d right Y: %d\n", left_x, left_y, right_x, right_y);
+  delay(10);
 }
